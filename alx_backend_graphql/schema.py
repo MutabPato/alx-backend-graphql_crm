b77bf6e40be61abdb92a06212c42e3cbcc907a0b
MutabPato <patrickmtbz@gmail.com>
2025-07-01 22:46:40 +0300

    Changing files to macth checker requirements

---
+++ alx_backend_graphql/schema.py
@@ -0,0 +1,10 @@
+#!/usr/bin/ python3
+import graphene
+
+class Query(graphene.ObjectType):
+        hello = graphene.String()
+        
+        def resolve_hello(root, info):
+              return "Hello, GraphQL!"
+    
+schema = graphene.Schema(query=Query)
